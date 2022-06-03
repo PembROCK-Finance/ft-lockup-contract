325ead1aa234b19b8ce4f5b6f2c886c66a92423d
HolyGrease <kireevdanil2000@gmail.com>
2022-06-03 17:27:11 +0300

    Add whitelist

--- src/owner.rs
+++ src/owner.rs
@@ -3,11 +3,25 @@ use near_sdk::near_bindgen;
 
 #[near_bindgen]
 impl Contract {
-	pub fn add_to_whitelist(&mut self, values: Vec<(AccountId, pool_id)>) {
+    pub fn add_to_whitelist(&mut self, values: Vec<(AccountId, u64)>) {
+        assert!(
+            self.deposit_whitelist
+                .contains(&env::predecessor_account_id()),
+            "Not allowed"
+        );
 
-	}
+        self.whitelisted_tokens.extend(values);
+    }
 
-	pub fn remove_from_whitelist(&mut self, values: Vec<(AccountId, pool_id)>) {
-		
-	}
+    pub fn remove_from_whitelist(&mut self, values: Vec<(AccountId, u64)>) {
+        assert!(
+            self.deposit_whitelist
+                .contains(&env::predecessor_account_id()),
+            "Not allowed"
+        );
+
+        values.iter().for_each(|value| {
+            self.whitelisted_tokens.remove(value);
+        });
+    }
 }
